7fed981835aaa18f9ebd3ee921684c263b3d2457
Abigail Gervase <agervase@mtb.com>
2022-05-17 13:12:43 -0400

    add dob slider

--- src/Components/DobInput/DobInput.tsx
+++ src/Components/DobInput/DobInput.tsx
@@ -1,11 +1,50 @@
 import React from "react";
+import Slider from "@material-ui/core/Slider";
+import { CustomSlider, Container } from "./DobInput.css";
 
 // slider selection
 export const DobInput = () => {
+  const JanFirst1900 = 44698;
+
+  function getFormattedDate(date: Date) {
+    var year = date.getFullYear();
+
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : "0" + month;
+
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : "0" + day;
+
+    return month + "/" + day + "/" + year;
+  }
+
+  function valueLabelFormat(value: number) {
+    var d = new Date();
+    d.setDate(d.getDate() - JanFirst1900 + value);
+    return `${getFormattedDate(d)}`;
+  }
+
+  const [value, setValue] = React.useState<number>(10);
+
+  const handleChange = (event: any, newValue: number | number[]) => {
+    if (typeof newValue === "number") {
+      setValue(newValue);
+    }
+  };
+
   return (
-    <div>
-      <label>Date of Birth:</label>
-      <input type="date" id="date" name="date" />
-    </div>
+    <Container>
+      <label>Date of Birth:</label> {valueLabelFormat(value)}
+      <CustomSlider
+        value={value}
+        min={0}
+        max={JanFirst1900}
+        getAriaValueText={valueLabelFormat}
+        valueLabelFormat={valueLabelFormat}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        aria-labelledby="non-linear-slider"
+      />
+    </Container>
   );
 };
