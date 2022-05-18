0f308a32186be74aa6c64ebf1caaddfb1177aa79
Abigail Gervase <agervase@mtb.com>
2022-05-18 16:13:47 -0400

    phone numbers

--- src/Components/PhoneInput/PhoneInput.tsx
+++ src/Components/PhoneInput/PhoneInput.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Select from "react-select";
-import { Container, NumbersContainer } from "./PhoneInput.css";
+import { ButtonContainer } from "../Form/Form.css";
+import {
+  ColumnContainer,
+  Container,
+  Header,
+  NumbersContainer,
+  RowContainer,
+} from "./PhoneInput.css";
 
 // dropdowns for each number
 export const PhoneInput = () => {
@@ -16,75 +23,72 @@ export const PhoneInput = () => {
     { value: "9", label: "9" },
     { value: "0", label: "0" },
   ];
+  const [value, setValue] = React.useState<number>(0);
+  const [phoneNumber, setPhoneNumber] = React.useState<string>("");
+
+  const handleClick = (input: number) => {
+    setValue(value + 1);
+    if (phoneNumber.length === 0) {
+      setPhoneNumber(phoneNumber + "(" + input.toString());
+    } else if (phoneNumber.length === 4) {
+      setPhoneNumber(phoneNumber + ") " + input.toString());
+    } else if (phoneNumber.length === 9) {
+      setPhoneNumber(phoneNumber + "-" + input.toString());
+    } else if (phoneNumber.length < 14) {
+      setPhoneNumber(phoneNumber + input.toString());
+    }
+  };
 
   return (
     <Container>
-      <label>Phone Number:</label>
-      <NumbersContainer>
-        <div>{`(`}</div>
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="first"
-          options={numberOptions}
-        />
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="second"
-          options={numberOptions}
-        />
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="third"
-          options={numberOptions}
-        />
-        <div>{`)`}</div>
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="fourth"
-          options={numberOptions}
-        />
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="fifth"
-          options={numberOptions}
-        />
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="sixth"
-          options={numberOptions}
-        />
-        <div>{`-`}</div>
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="seventh"
-          options={numberOptions}
-        />
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="eighth"
-          options={numberOptions}
-        />
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="ninth"
-          options={numberOptions}
-        />
-        <Select
-          className="number"
-          defaultValue={numberOptions[0]}
-          name="tenth"
-          options={numberOptions}
-        />
-      </NumbersContainer>
+      <Header>
+        <div>
+          <label>Phone Number:</label> {phoneNumber}
+        </div>
+        <button type="button" onClick={() => setPhoneNumber("")}>
+          Reset
+        </button>
+      </Header>
+      <ColumnContainer>
+        <RowContainer>
+          <button type="button" onClick={() => handleClick((value + 1) % 10)}>
+            {(value + 1) % 10}
+          </button>
+          <button type="button" onClick={() => handleClick((value + 2) % 10)}>
+            {(value + 2) % 10}
+          </button>
+          <button type="button" onClick={() => handleClick((value + 3) % 10)}>
+            {(value + 3) % 10}
+          </button>
+        </RowContainer>
+        <RowContainer>
+          <button type="button" onClick={() => handleClick((value + 4) % 10)}>
+            {(value + 4) % 10}
+          </button>
+          <button type="button" onClick={() => handleClick((value + 5) % 10)}>
+            {(value + 5) % 10}
+          </button>
+          <button type="button" onClick={() => handleClick((value + 6) % 10)}>
+            {(value + 6) % 10}
+          </button>
+        </RowContainer>
+        <RowContainer>
+          <button type="button" onClick={() => handleClick((value + 7) % 10)}>
+            {(value + 7) % 10}
+          </button>
+          <button type="button" onClick={() => handleClick((value + 8) % 10)}>
+            {(value + 8) % 10}
+          </button>
+          <button type="button" onClick={() => handleClick((value + 9) % 10)}>
+            {(value + 9) % 10}
+          </button>
+        </RowContainer>
+        <RowContainer>
+          <button type="button" onClick={() => handleClick((value + 0) % 10)}>
+            {(value + 0) % 10}
+          </button>
+        </RowContainer>
+      </ColumnContainer>
     </Container>
   );
 };
