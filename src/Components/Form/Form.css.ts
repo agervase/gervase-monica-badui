0b4a5e88cffc2d07c6588a437a7e859b9a436cc8
Abigail Gervase <agervase@mtb.com>
2022-05-18 15:45:40 -0400

    mobing button

--- src/Components/Form/Form.css.ts
+++ src/Components/Form/Form.css.ts
@@ -1,6 +1,18 @@
 import styled from "@emotion/styled";
 
-export const SubmitButton = styled.input``;
+interface ButtonProps {
+  marginLeft: string;
+  marginTop: string;
+}
+
+export const SubmitButtonStyle = styled.input<ButtonProps>`
+  margin-left: ${(props) => props.marginLeft}px;
+  margin-top: ${(props) => props.marginTop}px;
+`;
+
+export const ButtonContainer = styled.div`
+  padding: 20px;
+`;
 
 export const FormContainer = styled.div`
   padding: 40px;
